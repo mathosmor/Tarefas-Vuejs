323cc41759d17a27339bcd342ef69253cda41c6e
Matheus <mathos_mor@hotmail.com>
2019-04-30 16:31:39 -0300

    Teste do primeiro commit, tipo: comitando primeiro post

---
+++ src/components/TaskProgress.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="tasks-progress">
+        <span class="progress-value">{{ progress }}%</span>
+        <div class="progress-bar" :style="{width: progress + '%'}"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        progress: { type: Number, default: 0}
+    }
+}
+</script>
+
+<style>
+
+    .tasks-progress {
+        position: relative;
+        display: flex;
+        width: 80%;
+        border: 1px solid #fff;
+        color: #FFF;
+        border-radius: 8px;
+        margin-bottom: 15px;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .progress-bar{
+        position: absolute;
+        border-radius: 8px;
+        height: 100%;
+        background-color: #4caf50;
+        align-self: flex-start;
+
+    }
+
+    .progress-value {
+        z-index: 1;
+        font-size: 1.5rem;
+    }
+
+</style>
